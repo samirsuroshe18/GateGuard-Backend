21cfab0a471bb1ff3edf518b93183dd578a02c20
samir suroshe <sameersuroshe50@gmail.com>
2024-09-05 22:24:08 +0530

    changed add extra info controller

--- src/utils/sendResidentNotification.js
+++ src/utils/sendResidentNotification.js
@@ -1,26 +1,41 @@
 import admin from 'firebase-admin';
 
-const sendNotification = (token, role) => {
+const sendNotification = (fcmToken) => {
   const message = {
+    token: fcmToken,
     notification: {
-      title: 'New User Role Selection',
-      body: `A user has selected the role: ${role}`,
-      imageUrl: 'https://lh3.googleusercontent.com/a/ACg8ocJLc-iN0blZ8C0zfC9IzhmY4lGnW0onGwtm-PleG9sdqTHzdeCP=s96-c', // URL to your image
+      title: "New Request",
+      body: "You have a new approval request.",
     },
     data: {
-      click_action: 'FLUTTER_NOTIFICATION_CLICK',
-      action_approve: 'APPROVE_ACTION',
-      action_reject: 'REJECT_ACTION',
+      click_action: "FLUTTER_NOTIFICATION_CLICK",
     },
-    token: 'cy50UuLwTJuhuzy2PNq1gu:APA91bECAQ1mkSRSYTjptf1bylVI7ifeoSanxz-jFfzqSTcMr0TMFYf-J_VpeVBjOmiZQ3OxnhZ_aIy0WsYJXW5a8Q7L2FpbFk8gS1J2IQBNZ3f8GC2z_hfQ-bRl_j8tHA9G9jhkMS2v',
+    android: {
+      notification: {
+        title: "New Request",
+        body: "Approve or Reject the request.",
+        // Configure action buttons
+        actions: [
+          { title: "Approve", action: "APPROVE" },
+          { title: "Reject", action: "REJECT" }
+        ]
+      },
+    },
+    apns: {
+      payload: {
+        aps: {
+          category: "NEW_REQUEST",
+        },
+      },
+    },
   };
 
   admin.messaging().send(message)
     .then((response) => {
-      console.log('Successfully sent notification:', response);
+      console.log('Successfully sent message:', response);
     })
     .catch((error) => {
-      console.log('Error sending notification:', error);
+      console.log('Error sending message:', error);
     });
 };
 
