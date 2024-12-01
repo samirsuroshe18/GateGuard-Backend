0dfda797dbcbc9d1739437fba4186811ebb00316
samir suroshe <sameersuroshe50@gmail.com>
2024-12-01 22:31:08 +0530

    remove stringyfy method

--- src/utils/sendResidentNotification.js
+++ src/utils/sendResidentNotification.js
@@ -1,28 +1,10 @@
 import admin from 'firebase-admin';
 
-// const sendNotification = (token, action, payload) => {
-//   const message = {
-//     data: {
-//       action: action,  //'USER_ROLE_SELECTION' Optional: use it if you want to distinguish the notification type
-//       payload: payload,  // Custom data, like role or any other data you want to send
-//     },
-//     token: token,
-//   };
-
-//   admin.messaging().send(message)
-//     .then((response) => {
-//       console.log('Successfully sent notification:', response);
-//     })
-//     .catch((error) => {
-//       console.log('Error sending notification:', error);
-//     });
-// };
-
 const sendNotification = (token, action, payload) => {
   const message = {
     data: {
       action: action,  // 'USER_ROLE_SELECTION' Optional: use it if you want to distinguish the notification type
-      payload: JSON.stringify(payload),  // Custom data, like role or any other data you want to send
+      payload: payload,  // Custom data, like role or any other data you want to send
     },
     token: token,
     android: {
