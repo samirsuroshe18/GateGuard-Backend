fa1865f9226b8ebbe1696485f125e2d94b283ad5
samir suroshe <sameersuroshe50@gmail.com>
2024-12-01 21:06:06 +0530

    Added android.priority: high to ensure immediate delivery on Android devices.

--- src/utils/sendResidentNotification.js
+++ src/utils/sendResidentNotification.js
@@ -1,12 +1,38 @@
 import admin from 'firebase-admin';
 
+// const sendNotification = (token, action, payload) => {
+//   const message = {
+//     data: {
+//       action: action,  //'USER_ROLE_SELECTION' Optional: use it if you want to distinguish the notification type
+//       payload: payload,  // Custom data, like role or any other data you want to send
+//     },
+//     token: token,
+//   };
+
+//   admin.messaging().send(message)
+//     .then((response) => {
+//       console.log('Successfully sent notification:', response);
+//     })
+//     .catch((error) => {
+//       console.log('Error sending notification:', error);
+//     });
+// };
+
 const sendNotification = (token, action, payload) => {
   const message = {
     data: {
-      action: action,  //'USER_ROLE_SELECTION' Optional: use it if you want to distinguish the notification type
-      payload: payload,  // Custom data, like role or any other data you want to send
+      action: action,  // 'USER_ROLE_SELECTION' Optional: use it if you want to distinguish the notification type
+      payload: JSON.stringify(payload),  // Custom data, like role or any other data you want to send
     },
     token: token,
+    android: {
+      priority: "high", // Set high priority for Android
+    },
+    apns: {
+      headers: {
+        "apns-priority": "5", // Set high priority for iOS
+      },
+    },
   };
 
   admin.messaging().send(message)
@@ -17,6 +43,7 @@ const sendNotification = (token, action, payload) => {
       console.log('Error sending notification:', error);
     });
 };
+
 
 // Function to send a cancel notification (example for FCM)
 function sendNotificationCancel(token, payload) {
